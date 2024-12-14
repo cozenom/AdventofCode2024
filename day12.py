5e1304ec7ae8e1ea0e785e384f1b3f2622602b75
cozenom <david.p646@gmail.com>
2024-12-14 14:25:19 -0800

    day12p2

--- day12.py
+++ day12.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from collections import deque
 
 data = open("day12.txt").read()
@@ -16,7 +15,7 @@ data = open("day12.txt").read()
 # MMMISSJEEE
 # """
 
-data =  data.strip().split('\n')
+data = data.strip().split('\n')
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Part 1
@@ -30,44 +29,45 @@ visited = set()
 groups = []
 dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+
 # Find groups
 def bfs(startpos):
-    print(startpos)
     startx, starty = startpos
     startletter = data[starty][startx]
     current_group = set()
     queue = deque([startpos])
 
     while queue:
-        x,y = queue.popleft()
-        if (x,y) in visited:
+        x, y = queue.popleft()
+        if (x, y) in visited:
             continue
-        visited.add((x,y))
-        current_group.add((x,y))
+        visited.add((x, y))
+        current_group.add((x, y))
 
         for dx, dy in dirs:
             newx, newy = x + dx, y + dy
-            if 0<=newx<w and 0<=newy<h and data[newy][newx]==startletter and (newx,newy) not in visited:
-                queue.append((newx,newy))
+            if 0 <= newx < w and 0 <= newy < h and data[newy][newx] == startletter and (newx, newy) not in visited:
+                queue.append((newx, newy))
     return current_group
+
 
 for y in range(h):
     for x in range(w):
-        if (x,y) not in visited:
-            groups.append(bfs((x,y)))
+        if (x, y) not in visited:
+            groups.append(bfs((x, y)))
 
 
-# Find prod of area and perimiter of groups
+# Find prod of area and perimeter of groups
 
 def getperim(group):
     perimeter = 0
-
     for x, y in group:
         for dx, dy in dirs:
             newx, newy = x + dx, y + dy
             if not (0 <= newx < h and 0 <= newy < w) or (newx, newy) not in group:
                 perimeter += 1
     return perimeter
+
 
 res = 0
 for group in groups:
@@ -76,11 +76,48 @@ for group in groups:
 print("Part one: ")
 print(res)
 
+
 # ----------------------------------------------------------------------------------------------------------------------
 # Part 2
 
 # Find prod of area and number of sides
-# [print(len(i), i) for i in groups]
+
+def check_same(pos, plant):
+    x, y = pos
+    if 0 <= x < w and 0 <= y < h:
+        if data[y][x] == plant: return True
+    return False
+
+
+def check_corners(pos):
+    x, y = pos
+    plant = data[y][x]
+    NW, W, SW, N, S, NE, E, SE = check_same((x - 1, y - 1), plant), check_same((x - 1, y), plant), check_same(
+        (x - 1, y + 1), plant), check_same((x, y - 1), plant), check_same((x, y + 1), plant), check_same((x + 1, y - 1),
+                                                                                                         plant), check_same(
+        (x + 1, y), plant), check_same((x + 1, y + 1), plant)
+
+    edges = sum([
+        N and E and not NE,
+        N and W and not NW,
+        S and E and not SE,
+        S and W and not SW,
+        not (N or W),
+        not (W or S),
+        not (S or E),
+        not (E or N)
+    ])
+    return edges
+
+
+res = 0
+for group in groups:
+    seen = set()
+    corners = 0
+    plant = None
+    for pos in group:
+        corners += check_corners(pos)
+    res += len(group) * corners
 
 print("Part two: ")
-print()
+print(res)
